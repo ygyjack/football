9f2078eb4ded39054c765cff0a2030f98198162c
jack yu <jackyu@Keenansystem.com>
2017-10-27 10:31:28 +0100

    OSOL Code upload for config deploy5

--- server.js
+++ server.js
@@ -484,10 +484,10 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
     
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+//Object.assign=require('object-assign')
+//
+//app.engine('html', require('ejs').renderFile);
+//app.use(morgan('combined'))
 //
 //var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 //    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
